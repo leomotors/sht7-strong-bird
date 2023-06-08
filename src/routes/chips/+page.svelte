86d165b6bc78995df35b58da8e2940e47e3657b3
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-06-08 21:01:26 +0700

    chore: deps

---
+++ src/routes/chips/+page.svelte
@@ -0,0 +1,21 @@
+<h1 class="font-bold text-5xl">CHIPS Challenge</h1>
+
+<p class="text-red-500 font-bold text-5xl">ในนี้มีตั๋วกี่ใบไม่รู้ซ่อนอยู่</p>
+
+<iframe
+  width="560"
+  height="315"
+  src="https://www.youtube.com/embed/iGFWo65RrZE"
+  title="YouTube video player"
+  frameborder="0"
+  allowfullscreen
+/>
+
+<detail>
+  <summary>คำใบ้</summary>
+
+  <img
+    src="https://www.thebangkokinsight.com/wp-content/uploads/2021/03/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B4%E0%B8%95%E0%B8%A3-10364.png"
+    alt="รูปอะไรไม่รู้"
+  />
+</detail>
